5b3537c53cac2f1bcb7ee712d4f2999433365494
Jacastro619 <jorgecastro619@gmail.com>
2023-09-28 15:13:15 -0700

    first commit with proper files, and added pseudocode to js file

---
+++ assets/javascript/script.js
@@ -0,0 +1,25 @@
+// variable declarations
+// querySelectors
+
+// function to start the game
+// listen for start button click
+// start timer
+// first question displays
+// list of all answers
+// add event listiner to answer buttons
+
+// check to see if correct answer is selected with a function
+//compare clicked button to the correct answer stored
+// if wrong show wrong, and take time off timer
+// if correct show correct
+// goes to next question with the use of a function
+// If end of questions, end the game
+// if time runs out, end the game
+
+// end game
+// stop timer
+// display score which is the time remaining
+// have a form for initials and save score after initials have been submitted
+// clear timer
+
+//start again button that restarts the quiz
