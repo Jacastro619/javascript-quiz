cc8d63b42865c5c322fd2b05da055e36f85a7901
Jacastro619 <jorgecastro619@gmail.com>
2023-09-28 22:22:13 -0700

    add questions into array. add working timer function for timer functionality of the quiz app

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,80 @@
 // variable declarations
 // querySelectors
+var startCountDown = 90
+var timer = document.querySelector("#time")
+var startButton = document.querySelector(".start-button");
+var questionArray = [
+    question1 = {
+        question: "What is JavaScript primarily used for?",
+        correctAnswer: "Adding interactivity to web pages",
+        incorrectAnswer1: "Styling web pages",
+        incorrectAnswer2: "Creating web page layouts",
+        incorrectAnswer3: "Sending emails" 
+    },
+    question2 = {
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        correctAnswer: "var",
+        incorrectAnswer1: "variable",
+        incorrectAnswer2: "assign",
+        incorrectAnswer3: "delcare"
+    },
+    question3 = {
+        question: "What is the result of 10 + '10' in JavaScript?",
+        correctAnswer: "'1010'",
+        incorrectAnswer1: "20",
+        incorrectAnswer2: "10 + 10",
+        incorrectAnswer3: "'20'",
+    },
+    question4 = {
+        question: "What is a valid way to comment in JavaScript?",
+        correctAnswer: "// comment",
+        incorrectAnswer1: "**comment**",
+        incorrectAnswer2: "<!--comment-->",
+        incorrectAnswer3: "::comment::",
+    }, 
+    question5 = {
+        question: "Which JavaScript function is used to output data to the console for debuggin?",
+        correctAnswer: "console.log()",
+        incorrectAnswer1: "print()",
+        incorrectAnswer2: "display()",
+        incorrectAnswer3: "output()",
+    },
+    question6 = {
+        question: "What does the '===' operator in JavaScript check for?",
+        correctAnswer: "Both value and type equality",
+        incorrectAnswer1: "Value equality",
+        incorrectAnswer2: "Type equality",
+        incorrectAnswer3: "Inequality",
+    },
+    question7 = {
+        question: "What is '[ 2, 4, 6, 8]' an example of?",
+        correctAnswer: "Array",
+        incorrectAnswer1: "Object",
+        incorrectAnswer2: "String",
+        incorrectAnswer3: "List",
+    },
+    question8 = {
+        question: "What is an example of camelcase?",
+        correctAnswer: "camelCase",
+        incorrectAnswer1: "Camelcase",
+        incorrectAnswer2: "CamelCase",
+        incorrectAnswer3: "CamelcasE",
+    }
+]
+
+function startTimer() {
+    setInterval(function() {
+        timer.textContent = startCountDown
+        startCountDown--
+    }, 1000)
+}
+
+
+function startQuiz() {
+    startTimer()
+}
+
+startButton.addEventListener("click", startQuiz)
 
 // function to start the game
 // listen for start button click
